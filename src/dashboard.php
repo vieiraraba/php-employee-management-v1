2aaf400e2511863ac938f3ad795e49882b355f34
dariogmzfdz <dariogomezfdz@gmail.com>
2022-06-02 10:03:51 +0200

    feat(login): added the login page with its php and its basic css

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,1 +1,36 @@
 <!-- TODO Main view or Employees Grid View here is where you get when logged here there's the grid of employees -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Dashboard</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid.min.js"></script>
+    <link rel="icon" href="./favicon.svg">
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid.min.css" />
+    <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jsgrid/1.5.3/jsgrid-theme.min.css" />
+    <script type="module" src="../assets/js/index.js" defer></script>
+</head>
+
+<body>
+    <?php
+
+    session_start();
+
+    require_once("./library/sessionHelper.php");
+
+    checkSession(); // We check if the user has active login
+    checkSessionExpired(); // We check if the user session is still active
+
+    require_once("../assets/html/header.html");
+
+    ?>
+
+</body>
+
+</html>
