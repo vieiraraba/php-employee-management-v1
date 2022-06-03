c48a477304b1ed2c02f26c9d0bb18296ec6e7f06
dariogmzfdz <dariogomezfdz@gmail.com>
2022-06-03 13:38:40 +0200

    feat(put): now the users updated in the jsgrid are updated in the json file

--- src/library/employeeController.php
+++ src/library/employeeController.php
@@ -7,12 +7,13 @@ switch ($_SERVER["REQUEST_METHOD"]) {
         addEmployee($_POST);
         break;
     case 'PUT':
-
+        parse_str(file_get_contents("php://input"), $put);
+        updateEmployee($put);
         break;
     case 'DELETE':
         // Get the database connection file
-        parse_str(file_get_contents("php://input"), $delete_vars);
-        deleteEmployee($delete_vars["id"]);
+        parse_str(file_get_contents("php://input"), $delete);
+        deleteEmployee($delete["id"]);
         break;
     default:
         break;
--- src/library/employeeManager.php
+++ src/library/employeeManager.php
@@ -66,13 +66,27 @@ function deleteEmployee(string $id)
 }
 
 
-/* function updateEmployee(array $updateEmployee)
+function updateEmployee(array $updateEmployee)
 {
+    // Get the database connection file
     $jsonData = file_get_contents('../../resources/employees.json');
+    // Convert the JSON string to a PHP array
+    $data = json_decode($jsonData, true);
+    foreach ($data as $user) {
+        // Check if the employee already exists in the database
+        if ($user['id'] == $updateEmployee['id']) {
+            $updateEmployee = strToNumber($updateEmployee);
+            array_splice($data, array_search($user, $data), 1, [$updateEmployee]);
+        }
+    }
+    // Convert the array to a JSON string
+    $jsonData = json_encode($data, JSON_PRETTY_PRINT);
+    // Save the JSON string to the database file
+    file_put_contents('../../resources/employees.json', $jsonData);
 }
 
 
-function getEmployee(string $id)
+/* function getEmployee(string $id)
 {
 }
 
